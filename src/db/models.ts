edc949bbc95d03f2c46ef5e2e612b3b807b6237c
Marco Levrero <marco.levrero@gmail.com>
2022-01-28 20:55:20 +0100

    update model

--- src/db/models.ts
+++ src/db/models.ts
@@ -9,7 +9,7 @@ export enum Diet {
 
 export const party = schema({
   email: types.string({ required: true }),
-  password: types.string({ required: true }),
+  code: types.string({ required: true }),
   guests: types.array(
     types.object({
       name: types.string({ required: true }),
@@ -29,4 +29,4 @@ export type Guest = {
   comments?: string
 }
 
-export type Party = { email: string; password: string; guests: Guest[] }
+export type Party = { email: string; code: string; guests: Guest[] }
