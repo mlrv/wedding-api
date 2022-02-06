50b45537c60bf314956dedcdc24c52cc27628a0c
Marco Levrero <marco.levrero@gmail.com>
2022-02-06 18:23:32 +0100

    finalise email html

--- src/nodemailer/content.ts
+++ src/nodemailer/content.ts
@@ -20,7 +20,7 @@ export const RSVP_CONFIRMATION_SUBJECT_GB = 'Olivia & Marco - RSVP Confirmation'
 export const RSVP_CONFIRMATION_SUBJECT_IT = 'Olivia & Marco - Conferma RSVP'
 
 export const RSV_CONFIRMATION_HEADER_GB =
-  'Thank you so much for responding to our RSVP'
+  'Thank you so much for responding to our RSVP!'
 
 export const RSV_CONFIRMATION_HEADER_IT =
   'Grazie per aver risposto al nostro RSVP!'
@@ -47,10 +47,10 @@ export const RSVP_CONFIRMATION_HTML_COMING_IT = table(
 
 export const RSVP_CONFIRMATION_HTML_NOT_COMING_GB = table(
   RSV_CONFIRMATION_HEADER_GB,
-  'We are really sorry to hear that you won\'t be able to make it, and we hope that we\'ll ',
+  'We are really sorry to hear that you won\'t be able to make it. We\'ll keep you in our thoughts and hope to see you soon!',
 )
 
 export const RSVP_CONFIRMATION_HTML_NOT_COMING_IT = table(
   RSV_CONFIRMATION_HEADER_IT,
-  'Ci dispiace molto che non riuscite a venire, speriamo di...',
+  'Ci dispiace molto che non riuscite a venire. Speriamo di riuscire a vederci presto in qualche altra occasione!',
 )
